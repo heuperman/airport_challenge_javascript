bc401b54d6867e8ca577912815f181e0308b1fc6
Elishka Flint <elishkaf@gmail.com>
2018-07-10 15:46:53 +0100

    TEST Add test for .planes methods

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -3,12 +3,20 @@ describe('airport.js', function() {
   var airport;
   var plane;
 
-  describe(".land", function() {
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane');
+  });
 
+  describe(".land", function() {
     it('instructs a plane to land', function() {
-      airport = new Airport('name');
-      plane = jasmine.createSpy('plane');
       expect(airport.land(plane)).toEqual(plane);
+    });
+  });
+  describe(".planes", function() {
+    it('returns an array of planes', function() {
+      airport.land(plane);
+      expect(airport.planes).toContain(plane);
     });
   });
 });
