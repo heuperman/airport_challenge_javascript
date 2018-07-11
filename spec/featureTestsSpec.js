f49d342ae34ef6a812fb1c2c85a638d92449660a
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-11 12:23:48 +0100

    FEATURE Add test to check if stormy weather prevents planes from taking off

--- spec/featureTestsSpec.js
+++ spec/featureTestsSpec.js
@@ -1,11 +1,12 @@
 describe("airport", function() {
   var airport;
   var plane;
-  
+
   beforeEach(function(){
     airport = new Airport();
     plane = new Plane();
   });
+
 // As an air traffic controller
 // So I can get passengers to a destination
 // I want to instruct a plane to land at an airport
@@ -14,7 +15,6 @@ describe("airport", function() {
     expect(airport.planes).toContain(plane);
   });
 
-
 // As an air traffic controller
 // So I can get passengers on the way to their destination
 // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
@@ -22,5 +22,19 @@ describe("airport", function() {
     airport.land(plane);
     airport.takeOff(plane);
     expect(airport.planes).not.toContain(plane);
+  });
+
+// As an air traffic controller
+// To ensure safety
+// I want to prevent takeoff when weather is stormy
+  describe("when it is stormy", function(){
+    beforeEach(function(){
+      spyOn(weather, 'isStormy').andReturn(true);
+    });
+
+    it('instructs a plane to take off', function() {
+      airport.land(plane);
+      expect( function(){ airport.takeOff(plane); }).toThrow(new Error("Cannot take off: weather is stormy"));
+    });
   });
 });
